918558bf55c7d0b302e577a890233ff01af77cd1
Frida Jacobsson <frida_jacobsson@live.se>
2022-05-05 15:18:04 +0200

    Implemented base classes for client and server

---
+++ src/main/java/ClientServer/Client/StartClient.java
@@ -0,0 +1,8 @@
+package main.java.ClientServer.Client;
+
+public class StartClient {
+
+    public static void main(String[] args) {
+        Connection connection = new Connection();
+    }
+}
---
+++ src/main/java/ClientServer/Shared/Book.java
@@ -0,0 +1,34 @@
+package main.java.ClientServer.Shared;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+
+    private int storeId;
+    private String name;
+    private String firstAuthor;
+    private int publishingYear;
+
+    public Book(int storeId, String name, String firstAuthor, int publishingYear) {
+        this.storeId = storeId;
+        this.name = name;
+        this.firstAuthor = firstAuthor;
+        this.publishingYear = publishingYear;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFirstAuthor() {
+        return firstAuthor;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+}
