57e7b1f25ec5c6a9fb04f53225f296b9f7973f6f
Frida Jacobsson <frida_jacobsson@live.se>
2022-05-12 09:45:38 +0200

    Smaller changes

--- src/main/java/ClientServer/Server/ServerController.java
+++ src/main/java/ClientServer/Server/ServerController.java
@@ -1,8 +1,8 @@
 package ClientServer.Server;
 
 public class ServerController {
-    private Server server;
+
     public ServerController (){
-        server = new Server(6890);
+        //Server server = new Server(6890);
     }
 }
