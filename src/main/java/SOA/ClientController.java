30c70db525445bf4db52c3c39066fa702c22ce0c
EricSimonson <cirre96@gmail.com>
2022-05-18 13:21:33 +0200

    small buggs now everything works as it should

--- src/main/java/SOA/ClientController.java
+++ src/main/java/SOA/ClientController.java
@@ -3,11 +3,17 @@ package SOA;
 public class ClientController {
 
     public static void main(String[] args) {
-        int nbrOfClients = 50;
+        int nbrOfClients = 1000;
+        ClientGetBooks thread = null;
         long init = System.currentTimeMillis();
         for (int i = 0; i < nbrOfClients; i++) {
-            ClientGetBooks cgb = new ClientGetBooks();
-            cgb.start();
+            thread = new ClientGetBooks();
+            thread.start();
+        }
+        try{
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         long end = System.currentTimeMillis();
         long elapsedTime = end - init;
